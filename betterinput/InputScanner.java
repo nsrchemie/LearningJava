a8331fdaaa2c99e78831eb1fdb13cc47f4bfe36b
nsrchemie <nsrchemie@gmail.com>
2017-02-11 15:57:53 -0500

    working on simple scanner nextint method and calculations

--- betterinput/InputScanner.java
+++ betterinput/InputScanner.java
@@ -9,6 +9,13 @@ public class InputScanner {
 	line = console.nextLine();
 	System.out.println("Your Input: " + line);
 	}
+
+	static void intdemo(){
+		int inch;
+		double cm;
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter length in inches to convert to centimters");
+	}
 	public static void main(String[] args) {
 	linedemo();
 	}
