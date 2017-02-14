ee135aa6a5c2ec6cdb0e57c86a5864a21c3e9b63
nsrchemie <nsrchemie@gmail.com>
2017-02-14 15:15:59 -0500

    slicing strings

---
+++ slicestring/CutString.java
@@ -0,0 +1,9 @@
+public class CutString {
+
+	public static void main(String[] args) {
+	System.out.println("Practicing slicing strings");
+	String name = "Actaea Racemosa";
+	System.out.println("The scientific binomial name for White Baneberry is: " + name);
+	String genus = name.substring(0,name.indexOf( " " ));
+}
+}
