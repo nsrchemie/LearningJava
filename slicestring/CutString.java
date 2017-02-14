c6cd7c5b70eab7dd6e6a138cfb6c9b9bb696ad43
nsrchemie <nsrchemie@gmail.com>
2017-02-14 16:14:56 -0500

    touching up format, trimming whitespace

--- slicestring/CutString.java
+++ slicestring/CutString.java
@@ -5,5 +5,7 @@ public class CutString {
 	String name = "Actaea Racemosa";
 	System.out.println("The scientific binomial name for White Baneberry is: " + name);
 	String genus = name.substring(0,name.indexOf( " " ));
+	String species = name.substring(name.indexOf( " " )).trim();
+	System.out.println("The genus is " + genus + " and the species is " + species);
 }
 }
