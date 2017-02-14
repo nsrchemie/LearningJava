6b0d7aa7cd90234627c02e5fc1b822e6ab0df3da
nsrchemie <nsrchemie@gmail.com>
2017-02-14 16:18:16 -0500

    initial setup for user input, conditional next

---
+++ numcompare/NumCompare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NumCompare {
+	public static void main(String[] args) {
+		int first;
+		int second;
+		int third;
+		Scanner console = new Scanner(System.in);
+		System.out.println("Is this working?");
+		first = console.nextInt();
+		System.out.println("you typed in " + first);
+
+	}
+}
