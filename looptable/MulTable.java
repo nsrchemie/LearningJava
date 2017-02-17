a61aa1dd8ae8d934f0b9c43da7841156cb916889
nsrchemie <nsrchemie@gmail.com>
2017-02-16 22:56:23 -0500

    improved generalization

--- looptable/MulTable.java
+++ looptable/MulTable.java
@@ -1,22 +1,22 @@
 public class MulTable {
 	public static void main(String[] args) {
-		printTable();
+		printTable(10);
 
 	}
 
-	static void printRow(int n) {
+	static void printRow(int n , int cols) {
 		int i = 1;
-		while (i <= 6) {
+		while (i <= cols) {
 			System.out.printf("%4d", n * i);
 			i = i + 1;
 		}
 		System.out.println();
 	}
 
-	static void printTable() {
+	static void printTable(int rows) {
 		int i = 1;
-		while (i <= 6) {
-			printRow(i);
+		while (i <= rows) {
+			printRow(i, rows);
 			i = i + 1;
 		}
 	}
