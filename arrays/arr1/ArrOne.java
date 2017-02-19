2834252073eeecfd05c9c65d31000a546c67bbd5
nsrchemie <nsrchemie@gmail.com>
2017-02-19 12:30:49 -0500

    demo creating array and printing default values of 0 with loop

---
+++ arrays/arr1/ArrOne.java
@@ -0,0 +1,10 @@
+public class ArrOne {
+	public static void main(String[] args) {
+		int[] counts;
+		counts = new int[4];
+		for (int i = 0; i < counts.length; i++) {
+			System.out.println(counts[i]);
+		}
+
+	}
+}
